6073edf92834c84860713436dab79e242bfa5d66
YutaGoto <you.goto.510@gmail.com>
2016-07-16 18:41:39 +0900

    init commit

---
+++ ConsoleMultiFiles/Prime/Prime.cs
@@ -0,0 +1,20 @@
+﻿namespace Prime
+{
+    internal class DecisionPrime
+    {
+        public bool IsPrime(int i)
+        {
+            if (i < 2)
+                return false;
+
+            int x = 2;
+            while (i > x)
+            {
+                if (i % x == 0)
+                    return false;
+                x++;
+            }
+            return true;
+        }
+    }
+}
---
+++ ConsoleMultiFiles/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMultiFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Prime.DecisionPrime pNum = new Prime.DecisionPrime();
+            Console.WriteLine("Please input integer number.");
+            int num = int.Parse(Console.ReadLine());
+            if (pNum.IsPrime(num))
+                Console.WriteLine(num.ToString() + " is prime number!\n");
+            else
+                Console.WriteLine(num.ToString() + " is NOT prime number!\n");
+
+            Console.WriteLine("press any key");
+            Console.ReadKey();
+        }
+    }
+}
